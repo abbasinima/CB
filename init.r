0416c46a8bb2cfed50560f97e9226b317128395d
abbasinima <n.abbasi7@yahoo.com>
2017-02-06 13:04:33 -0500

    Update init.r

--- init.r
+++ init.r
@@ -1,6 +1,6 @@
 ###########################################################
 # Update this line with the R packages to install:
 
-install.packages(c("plyr","stringr","data.table","rCharts","ggplot2","leaflet"),dependencies = TRUE)
+install.packages(c("plyr","stringr","data.table","rCharts","ggplot2"),dependencies = TRUE)
 
 ###########################################################
